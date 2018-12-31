0885dc280de87b709f2458765a43f86a5955b12d
craken <cesar.robles@digitalaid.com.mx>
2018-12-31 12:09:50 -0600

    farmtc vone

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; <?php echo date('Y')?> <a href="https://www.craken.digitalaid.com.mx" target="_blank">Sitio creado por Craken</a>.</strong>
+
+	<!-- Todos los derechos reservados. -->
+
+</footer>
